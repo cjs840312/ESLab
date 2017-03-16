9e1536d94c7f07c7017f6d26e95e9d93efdea54a
Jonathan <cjs840312@gmail.com>
2017-03-16 17:18:55 +0800

    homepage only

---
+++ Messenger/sever.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var url = require('url');
+var fs = require('fs');
+
+console.log('Server starts...')
+
+var server = http.createServer(function(request, response) {
+  console.log('Connection');
+  var path = url.parse(request.url).pathname;
+
+  switch (path) {
+    case '/':
+      fs.readFile(__dirname + '/HomePage.html', function(error, data) {
+        if (error){
+          response.writeHead(404);
+          response.write("HomePage doesn't exist - 404");
+        } else {
+          response.writeHead(200, {"Content-Type": "text/html"});
+          response.write(data, "utf8");
+        }
+        response.end();
+      });
+      break;
+    default:
+      response.writeHead(404);
+      response.write("opps this doesn't exist - 404");
+      response.end();
+      break;
+  }
+});
+
+server.listen(8001);
